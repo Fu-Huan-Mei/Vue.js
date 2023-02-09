6de9df650aed130878945f14361288d46432dc33
FuHuanMei <3533643117@qq.com>
2023-02-09 17:02:10 +0800

    代码分析

--- FirstLearning/February_9/vue-project/src/App.vue
+++ FirstLearning/February_9/vue-project/src/App.vue
@@ -1,47 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+//1、组件：
+//（1）含义：就是一个普通的js对象
+//（2）作用：一个组件可以创建多个组件实例
+  export default {
+//2、data
+//（1）本质：是一个函数/方法
+    data() {
+//（6）注意：直接向组件实例中添加的属性不会被vue所代理，不是响应式数据，修改后页面不会发生变化
+      this.name = "孙悟空"
+//（2）this：就是当前组件实例vm
+//（3）注意：如果使用箭头函数，则无法通过this来访问组件实例
+      console.log("data=",this)//Proxy
+//（4）返回值：返回一个对象，vue会对该对象进行代理，从而将其转换为响应式数据，响应式数据可以直接通过组件实例访问
+      return {
+        msg:"我爱vue"
+      }
+    }
+//（5）箭头函数的表示方式（不建议）
+    // data:vm => {
+    //   console.log("data=",vm)
+    //   return {
+    //     msg:"我爱vue"
+    //   }
+    // }
+  }
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ msg }}</h1>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- FirstLearning/February_9/vue-project/src/main.js
+++ FirstLearning/February_9/vue-project/src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
+//1、App.vue是根组件
 import App from './App.vue'
-
-import './assets/main.css'
-
-createApp(App).mount('#app')
+//2、createApp(App)将根组件关联到应用上
+// createApp(App).mount('#app')
+//3、返回应用实例
+const app = createApp(App)
+console.log(app)//Object
+//4、app.mount("#app")
+//（1）作用：将应用挂载到页面中
+//（2）返回值：会返回一个根组件的实例,组件的实例通常命名为vm,组件实例是一个Proxy对象（代理对象）
+const vm =  app.mount("#app")
+//vm本身在一个模块里，是局部变量
+console.log(vm)//Proxy
+//5、将vm设置为全局变量，就可直接在浏览器的控制台访问
+window.vm = vm

29d5959a3421bf6446fa537decc910e3e9c666ea
FuHuanMei <3533643117@qq.com>
2023-02-09 08:44:14 +0800

    提交学习内容1

---
+++ FirstLearning/February_9/auto_vue/src/App.vue
@@ -0,0 +1,12 @@
+<script>
+    export default {
+        data(){
+            return {
+                msg:"哈哈哈哈"
+            }
+        }
+    }
+</script>
+<template>
+    <h1>{{ msg }}</h1>
+</template>
